128828d5dd38c251e9517a6749f70639e7e50fb2
sidielbachir <sidielbachirguedda@gmail.com>
2024-07-16 11:58:04 +0100

    Guessing_Game.c

--- guessing game.c
+++ guessing game.c
@@ -4,15 +4,20 @@
 int main(void)
 {
 	int chosen=0;
-	srand(time(NULL));
-	int limit=20.0;
-	chosen=1+rand()%limit;
+	
+	int limit=20;
+	
 	int guess=0;
 	int count=0;
-	
+	char another_game='y';
 	printf("Hi,This is a guessing game!!!");
 	printf("\n\nI've chosen a number between 1 and 20"
 	              " ,which you must guess...");
+	printf("\nPress Enter To Start: ");
+	scanf("%c",&another_game);              
+	do{
+	srand(time(NULL));chosen=1+rand()%limit;
+	
     for(count=3;count>0;--count)
     {
     	printf("\nEnter your %s guess :",
@@ -20,15 +25,18 @@ int main(void)
 		scanf("%d",&guess);
 		if(guess==chosen)
 		{
-			printf("\nCongratulation,you guessed correctly *_*");
-			return 0;
+			printf("\n\tCongratulation,you guessed correctly *_*");
+			break;
 		}
-		if(guess<1 || guess>20)printf("\nI said within this range"
+		if(guess<1 || guess>20)printf("\tI said within this range"
 		                                 " 0<guess<=20");
-		else printf("\nSorry you guessed worng!!!");
-		
-	}
-	   printf("\nYour last guess is worng too!!!"
+		else printf("\tSorry you guessed worng!!!");
+		if(count==1) printf("\n\nYour last guess is worng too!!!"
 			                        "the answer is %d",chosen);
-	
+	}
+	printf("\n\n\tAnother round ?");scanf(" %c",&another_game);
+	   
+			                        
+    }while(another_game=='y');
+    return 0;
 }
